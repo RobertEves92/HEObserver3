41c10ce2d458998fd79e8d5061f206be57bfa4d4
Robert Eves <roberteves92@gmail.com>
2018-03-06 20:58:22 +0000

    add unescape class with method to remove escaped hex chars

---
+++ HertfordshireMercury/HertfordshireMercury/Services/Unescape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HertfordshireMercury.Services
+{
+    public static class Unescape
+    {
+        /// <summary>
+        /// Returns an unescaped string replacing any hexadecimal character references with their ASCII counterparts
+        /// </summary>
+        /// <param name="escaped">Escaped string to unescape</param>
+        /// <returns>Unescaped string</returns>
+        public static string UnescapeHexCharacters(string escaped)
+        {
+            string unescaped = escaped;
+            MatchCollection hexmatchs = Regex.Matches(escaped, "&#x[0-9][0-9];");
+
+            string[] unescapedMatches = new string[hexmatchs.Count];
+
+            for (int i = 0; i < hexmatchs.Count; i++)
+            {
+                string s = hexmatchs[i].Value;
+                s = s.Replace("&#x", "");
+                s = s.Replace(";", "");
+                s = Int16.Parse(s, System.Globalization.NumberStyles.AllowHexSpecifier).ToString();
+                unescapedMatches[i] = s;
+            }
+
+            for (int i = 0; i < hexmatchs.Count; i++)
+            {
+                unescaped = escaped.Replace(hexmatchs[i].Value, ((char)int.Parse(unescapedMatches[i])).ToString());
+            }
+
+            return unescaped;
+        }
+    }
+}
