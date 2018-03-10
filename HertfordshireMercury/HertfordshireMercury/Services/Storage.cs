0b25311f24bca1de43538ada3ab0cabac523840d
Robert Eves <roberteves92@gmail.com>
2018-03-10 06:57:52 +0000

    create new class Storage

---
+++ HertfordshireMercury/HertfordshireMercury/Services/Storage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HertfordshireMercury.Services
+{
+    public static class Storage
+    {
+        public static string StoragePath => Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+    }
+}
