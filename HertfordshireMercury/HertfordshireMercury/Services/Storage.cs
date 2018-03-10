c5f412eba62eaf907011f455914deef8bab9c49f
Robert Eves <roberteves92@gmail.com>
2018-03-10 07:38:49 +0000

    add method for saving xml feed to file

--- HertfordshireMercury/HertfordshireMercury/Services/Storage.cs
+++ HertfordshireMercury/HertfordshireMercury/Services/Storage.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
 
 namespace HertfordshireMercury.Services
 {
     public static class Storage
     {
         public static string StoragePath => Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+        public static void SaveXmlDoc(string DocSource, string Path)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(DocSource);
+            xml.Save(StoragePath + Path);
+        }
     }
 }
